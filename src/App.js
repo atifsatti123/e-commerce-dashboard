2ee4221c8ce8193c8bd342179f867e5c9223a7da
atifsatti123 <90699664+atifsatti123@users.noreply.github.com>
2022-10-14 22:12:29 +0500

    ecommdashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ECommerce from './components/ECommerce';
+
+function App() {
+  return (
+    <div className="App">
+      <ECommerce />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ECommerce.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const ECommerce = () => {
+  return (
+    <div>
+          <h1>E-Commerce Project</h1>
+          <button>Normal Button</button>
+          <Button>Bootstrap Button</Button>
+    </div>
+  )
+}
+
+export default ECommerce
